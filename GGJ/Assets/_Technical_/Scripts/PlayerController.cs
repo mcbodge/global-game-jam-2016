d5b067314886ed158bad12a5ac90b4552ad2cadb
luca <lucazamboni@MacBook-Pro.local>
2016-01-30 06:10:08 +0100

    function to lerp

---
+++ GGJ/Assets/_Technical_/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        ////float moveHorizontal = Input.GetAxis("Horizontal");
+        ////float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(5.0f, 0.0f, 0.0f);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ GGJ/Assets/_Technical_/Scripts/Trigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour
+{
+
+    public GameObject Camera;
+
+    public Vector3 FinalPosition;
+    private bool goLeft;
+    public float Speed;
+
+
+
+    void Update()
+    {
+        if (goLeft)
+        {
+            Camera.transform.position = Vector3.Lerp(Camera.transform.position, FinalPosition, Time.deltaTime * Speed);
+            if (Vector3.Distance(Camera.transform.position, FinalPosition) < 0.25f)
+                goLeft = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!goLeft) {
+            FinalPosition = Camera.transform.position;
+            FinalPosition.x += 10f;
+            ////Camera.transform.position = FinalPosition;
+            goLeft = true;
+        }
+    }
+
+}
