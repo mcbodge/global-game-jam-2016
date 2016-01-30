66187a03d0a97fe86b88f9f4a16669f125902ef9
mcbodge <manuel.grillo@mail.polimi.it>
2016-01-30 09:48:37 +0100

    Added scene with sphere

--- GGJ/Assets/_Technical_/Scripts/PlayerController.cs
+++ GGJ/Assets/_Technical_/Scripts/PlayerController.cs
@@ -4,22 +4,31 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
+    public float Speed;
+    public float JumpSpeed;
 
     private Rigidbody rb;
+    private bool IsGrounded;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        IsGrounded = false;
     }
 
     void FixedUpdate()
     {
-        ////float moveHorizontal = Input.GetAxis("Horizontal");
-        ////float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(5.0f, 0.0f, 0.0f);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+        rb.AddForce(movement * Speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            IsGrounded = false;
+            rb.AddForce(Vector3.up * JumpSpeed, ForceMode.Impulse);
+        }
     }
 }
