e07f08696d65a0fd432c4cb60b142a24dd9fa985
mcbodge <manuel.grillo@mail.polimi.it>
2016-01-30 22:34:16 +0100

    Added collectable object.

---
+++ GGJ/Assets/_Technical_/Scripts/CollectableObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableObject : MonoBehaviour {
+
+    public CollectLevel myControl;
+    public CollectableType type;
+
+    void OnTriggerEnter()
+    {
+        gameObject.SetActive(false);
+        if (type.Equals(CollectableType.Good))
+            myControl.CollectedObjects++;
+        else
+            myControl.CurrentPlayerController.StopPlayer(5f);
+    }
+}
